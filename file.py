9f581661fde2041690839714e89f48925cde8de1
milkyway03 <95718076+milkyway03@users.noreply.github.com>
2022-01-05 21:56:28 +0300

    Add files via upload

--- file.py
+++ file.py
@@ -2,14 +2,55 @@ import json
 
 
 def get_candidates():
-    with open("homework12/candidates.json", "r") as fp:
+    with open("data/candidates.json", "r", encoding="utf-8") as fp:
         candidates = json.load(fp)
     return candidates
 
 
+def get_candidate_by_id(can_id):
+    candidates = get_candidates()
+    for can in candidates:
+        if can_id == can["id"]:
+            return can
+
+
 def get_settings():
-    with open("homework12/settings.json", "r") as fp:
+    with open("data/settings.json", "r", encoding="utf-8") as fp:
         settings = json.load(fp)
     return settings
 
 
+def get_candidate_by_name(name):
+    candidates = get_candidates()
+    settings = get_settings()
+
+    candidates_found = []
+
+    for can in candidates:
+
+        if settings["case-sensitive"]:
+            if name in can["name"]:
+                candidates_found.append(can)
+        else:
+            if name.lower() in can["name"].lower():
+                candidates_found.append(can)
+
+    return candidates_found
+
+
+def get_candidate_by_skill(skill_name):
+    candidates = get_candidates()
+    settings = get_settings()
+
+    candidates_found = []
+
+    for can in candidates:
+
+        if settings["case-sensitive"]:
+            if skill_name in can["skills"]:
+                candidates_found.append(can)
+        else:
+            if skill_name.lower() in can["skills"].lower():
+                candidates_found.append(can)
+
+    return candidates_found
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from flask import Flask, request, render_template
-import json
-
-
-def get_candidates():
-    with open("homework12/candidates.json", "r") as fp:
-        candidates = json.load(fp)
-    return candidates
-
-
-def get_settings():
-    with open("homework12/settings.json", "r") as fp:
-        settings = json.load(fp)
-    return settings
+from file import get_settings, get_candidates, get_candidate_by_id, get_candidate_by_name, get_candidate_by_skill
 
 
 app = Flask(__name__)
@@ -25,6 +13,39 @@ def page_index():
     if online:
         return "Приложение работает"
     return "Приложение не работает"
+
+
+@app.route("/list")
+def page_list_of_candidates():
+
+    cans = get_candidates()
+    return render_template("list.html", cans=cans)
+
+
+@app.route('/candidate/<int:can_id>')
+def page_single_candidate(can_id):
+
+    can = get_candidate_by_id(can_id)
+
+    return render_template("candidates.html", can=can)
+
+
+@app.route('/search')
+def page_search_by_name():
+    name = request.args['name']
+
+    cans = get_candidate_by_name(name)
+    cans_count = len(cans)
+
+    return render_template("search.html", cans=cans, cans_count=cans_count)
+
+
+@app.route('/skill/<skill_name>')
+def page_search_by_skill(skill_name):
+    cans = get_candidate_by_skill(skill_name)
+    cans_count = len(cans)
+
+    return render_template("skill.html", skill_name=skill_name, cans=cans, cans_count=cans_count)
 
 
 app.run()
